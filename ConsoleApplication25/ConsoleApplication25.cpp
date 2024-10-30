3ccf91db8374d168e0d72d07be7dd52ac1d14544
Егор <@DESKTOP-VF4U8Q2>
2024-10-30 18:33:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication25/ConsoleApplication25.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    float initialAmplitude, finalAmplitude;
+
+    cout << "Начальная амплитуда: ";
+    cin >> initialAmplitude;
+
+    cout << "Конечная амплитуда: ";
+    cin >> finalAmplitude;
+
+    int oscillation = 0;
+    float currentAmplitude = initialAmplitude;
+
+    while (currentAmplitude > finalAmplitude) {
+        currentAmplitude *= (1 - 0.084f);
+        oscillation++;
+    }
+
+    cout << "Маятник будет качается " << oscillation << " перед тем как остановится";
+
+}
+
